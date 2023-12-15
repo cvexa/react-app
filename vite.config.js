6d46c3247bc9c929e836b9797a0fc1cdead34860
Светослав Василев <cvexxxxa@abv.bg>
2023-12-15 14:34:39 +0200

    firebase deploy installed, and PWA vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Cvexa react app',
+        short_name: 'cvexaReactApp',
+        description: 'My Awesome React description',
+        theme_color: '#ffffff',
+      }
+    })],
 })
